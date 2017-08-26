ecbf028f35f23a2f621db28fa96959e6accd2a6c
Ben Orchard <thefirstmuffinman@gmail.com>
2017-08-26 18:56:33 +0100

    Log: replace prefix with datetime

--- src/log.c
+++ src/log.c
@@ -2,10 +2,10 @@
 #include <pspkernel.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 static struct {
     log_level level;
-    char *prefix;
     char *out;
 } logger;
 
@@ -14,7 +14,6 @@ static struct {
 bool logging_init(log_level level, char *out) {
     logger.level = level;
     logger.out = out;
-    logger.prefix = "test: ";
 
     // check that the file exists (if not, create it) and that we can open &
     // write (append) to it
@@ -35,15 +34,22 @@ void logging_deinit() {
 }
 
 // TODO: efficiency concerns:
+//   * wasting memory with the fmt_now[100], use dynamic allocation/realloc (?)
 //   * could save strlen(log_str), it's used twice
 //   * strcat is slow
 bool log_msg(log_level level, char *msg) {
     // do nothing and return true if we're configured not to log anything
     if (logger.level == LOG_LEVEL_NONE) { return true; }
     else if (logger.level <= level) {
-        // allocate memory: prefix, msg, \n
-        char *log_str = malloc(strlen(logger.prefix) + strlen(msg) + 1);
-        strcpy(log_str, logger.prefix);
+        char fmt_now[200];
+        time_t t = time(NULL);
+        struct tm *tm = localtime(&t);
+        strftime(fmt_now, sizeof(fmt_now), "%FT%T%z ", tm);
+
+        // allocate memory: date + msg + \n
+        // TODO: could probably shave off a byte b/c strcat removes 1 null char
+        char *log_str = malloc(sizeof(fmt_now) + strlen(msg) + 1);
+        strcpy(log_str, fmt_now);
         strcat(log_str, msg);
         strcat(log_str, "\n");
 
